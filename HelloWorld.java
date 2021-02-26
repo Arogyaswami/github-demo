fca4f7446e95b5659f03d6e5c28ec75ab5f7c4fb
Madanu Arogyaswami <madanu.arogyaswami@wipro.com>
2021-02-26 18:33:53 +0530

    Making small changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Hello,World!\n");
 		System.out.println("Welcome to ENV training");
+	System.out.println("Hai Everyone");
 	}
 }
-
